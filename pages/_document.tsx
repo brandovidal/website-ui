5b15fe3de512e532bf3c74f3c5f661974568f376
Brando Vidal <yisusdracon@gmail.com>
2022-12-16 03:41:00 -0500

    🔦 add TS Standard Linter
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
-export default function Document() {
+export default function Document (): JSX.Element {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
